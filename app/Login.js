922bfb14e919ff31591649738e36e8b1f53a8aa2
Jihye <wlgul817@gmail.com>
2019-07-08 23:32:50 +0900

    190708 loginpage
    doing loginpage

--- app/Login.js
+++ app/Login.js
@@ -7,13 +7,35 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 
 export default class Login extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <View style={styles.whitebox}>
+          <View>
+            <Text>HAT</Text>
+          </View>
+
+          <View>
+            <Text>ID</Text>
+            <TextInput style={styles.inputbox} />
+          </View>
+
+          <View>
+            <Text>PASSWORD</Text>
+            <TextInput style={styles.inputbox} />
+          </View>
+          
+          <TouchableOpacity style={styles.loginbtn}><Text>LOGIN</Text></TouchableOpacity>
+          
+          <TouchableOpacity style={[styles.loginbtn, {backgroundColor:'#19C959'}]}><Text>네이버 아이디로 로그인</Text></TouchableOpacity>
+        </View>
+        <View style={styles.bottom}>
+          <TouchableOpacity><Text>ID/PW찾기</Text></TouchableOpacity>
+          <TouchableOpacity><Text>회원가입</Text></TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -24,16 +46,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FF7C5E',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  whitebox: {
+    backgroundColor: '#FFF',
+    width: '80%',
+    height:'70%',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  inputbox: {
+    backgroundColor: '#CCC'
   },
+  loginbtn: {
+    width: '80%',
+    paddingVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF7C5E'
+  },
+  bottom: {
+    width: '80%',
+  }
 });
