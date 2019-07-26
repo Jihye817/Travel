4dd99125c7ac75d0dc283df371138f92bdfed426
Jihye <wlgul817@gmail.com>
2019-07-27 00:00:34 +0900

    Update Triplist_main.js

--- app/pages/Triplist_main.js
+++ app/pages/Triplist_main.js
@@ -22,12 +22,24 @@ export default class Triplist_main extends Component{
     getTripFunction(email) {
         tripFunc.GetTrips(email).then(function(response) {
             console.log(email);
-            return response;
+            //return response;
+            return JSON.parse(response)
         })
         .then((data) => {
             this.setState({tripData : data})
             console.log(this.state.tripData) //data가 제대로 나오는지 확인하기 위함
-        });
+        }).done();
+    }
+
+    areaFunction(key){
+        switch(key) {
+            case 1:
+                return '서울';
+            case 2:
+                return '인천';
+            case 7:
+                return '울산';
+        }
     }
 
     _renderItem =({item}) => {//Flatlist list 내용
@@ -37,22 +49,26 @@ export default class Triplist_main extends Component{
                     <View style = {styles.listwrap}>
                         <View style={styles.topwrap}>
                             <View style={styles.txtrowcenter}>
-                                <Image style={{height:32, width:32}} resizeMode='contain' source={require('../assets/images/location_orange.png')}></Image>
-                                <Text style={{fontSize: 32}}> 제목</Text>
-                            </View>
-                            <View style={styles.txtrowcenter}>
-                                <Text style={{fontSize: 24, fontWeight: '400'}}>4</Text>
-                                <Text style={{fontSize: 24, fontWeight: '400'}}>/</Text>
-                                <Text style={{fontSize: 24, fontWeight: '400'}}>4</Text>
-
-                                <Text style={{fontSize: 24, fontWeight: '400', color:'#FF7C5E'}}>~</Text>
-
-                                <Text style={{fontSize: 24, fontWeight: '400'}}>4</Text>
-                                <Text style={{fontSize: 24, fontWeight: '400'}}>/</Text>
-                                <Text style={{fontSize: 24, fontWeight: '400'}}>5</Text>
+                                <Image style={{height:30, width:30, marginRight:10}} resizeMode='contain' source={require('../assets/images/location_orange.png')}></Image>
+                                <Text ellipsizeMode='tail' numberOfLines={1} style={{fontSize: 30}}>{item.name}</Text>
                             </View>
                         </View>
-                        <View style={{marginLeft:30,}}><Text>위치, 위치, 위치</Text></View>
+                        <View style={[styles.txtrowcenter, {marginLeft:30, marginVertical:5}]}>
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>{item.start.substring(2,4)}</Text>
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>/</Text>
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>{item.start.substring(5,7)}</Text>
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>/</Text>
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>{item.start.substring(8,10)}</Text>
+
+                                <Text style={{fontSize: 22, fontWeight: '400', color:'#FF7C5E'}}>~</Text>
+
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>{tripFunc.ConvertDate(item.end)}</Text>
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>/</Text>
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>{item.end.substring(5,7)}</Text>
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>/</Text>
+                                <Text style={{fontSize: 22, fontWeight: '400'}}>{item.end.substring(8,10)}</Text>
+                            </View>
+                        <View style={{marginLeft:30,}}><Text>{this.areaFunction(item.area1)}, {item.area2},{item.area3}</Text></View>
                         <View style={{alignItems:'flex-end'}}>
                             <TouchableOpacity style={{width: 50, padding:5, backgroundColor: '#FF7C5E', justifyContent:'center', alignItems:'center'}}>
                                 <Text style={{color:'#FFF'}}>삭제</Text>
@@ -131,7 +147,9 @@ const styles = StyleSheet.create({
     topwrap: {
         flexDirection: 'row',
         alignItems:'center',
-        justifyContent:'space-between'
+        justifyContent:'space-between',
+        borderBottomWidth:1,
+        borderBottomColor:'#E2E2E2'
     },
     txtrowcenter: {
         flexDirection:'row',
