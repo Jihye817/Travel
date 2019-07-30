46f011d2134f19f1b75d308f0fc3a723702baef5
Heyoung Ahn <hyeyoung0112@gmail.com>
2019-07-30 10:56:11 +0900

    multiple area accepted

--- serverRequest/info_request.js
+++ serverRequest/info_request.js
@@ -5,22 +5,62 @@ GetInfoTypeArea 함수 설명
 tour, eat, stay 타입에서 해당 지역의 장소들의 이름, 주소 반환
 type: 'tour' / 'eat' / 'stay'
 ******************************/
-export function GetInfoTypeArea(type, area) {
-    return new Promise(function(resolve, reject) {
-        request({
-            url: "http://106.10.53.87:8080/info/area/" + type + '/' + area,
-            method: "GET"
-        }, function (error, response, body){
-            if (error) {
-                console.log(error);
-                resolve(null);
-            }
-            else {
-                resolve(body);
-            }
+export function GetInfoTypeArea(type, area1, area2=null, area3=null) {
+        var promise1 = new Promise(function(resolve, reject) {
+            request({
+                url: "http://106.10.53.87:8080/info/area/" + type + '/' + area1,
+                method: "GET"
+            }, function (error, response, body){
+                if (error) {
+                    console.log(error);
+                    resolve(null);
+                }
+                else {
+                    resolve(body);
+                }
+            })
         });
-    });
+        var promise2 = new Promise(function(resolve, reject) {
+            if (area2 != null) {
+            request({
+                url: "http://106.10.53.87:8080/info/area/" + type + '/' + area2,
+                method: "GET"
+            }, function (error, response, body){
+                if (error) {
+                    console.log(error);
+                    resolve(null);
+                }
+                else {
+                    resolve(body);
+                }
+            });
+            }
+            else resolve([]);
+        });
+        var promise3 = new Promise(function(resolve, reject) {
+            if (area3 != null) {
+            request({
+                url: "http://106.10.53.87:8080/info/area/" + type + '/' + area3,
+                method: "GET"
+            }, function (error, response, body){
+                if (error) {
+                    console.log(error);
+                    resolve(null);
+                }
+                else {
+                    resolve(body);
+                }
+            });
+            }
+            else resolve([]);
+        });
+
+        return Promise.all([promise1, promise2, promise3], function(values) {
+            var finalArr = (values[0].concat(values[1])).concat(values[2]);
+            resolve(finalArr);
+        });
 }
+
 
 /******************************
 GetFullInfoTypeID 함수 설명
@@ -66,9 +106,10 @@ export function GetInfoTypeID(type, id) {
 
 //module.exports=[GetInfoTypeArea, GetFullInfoTypeID, GetInfoTypeID];
 
+
+//Example Code
 /*
-Example Code
-GetInfoTypeArea('eat', 7).then(function(data) {
+GetInfoTypeArea('tour', 1).then(function(data) {
     console.log(data);
 });
 
